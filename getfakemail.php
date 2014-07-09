a36c7630cda9377de0332dd930ca7719b1cded3b
Mitch Amiano <mcamiano@ncsu.edu>
2014-07-09 10:57:11 -0400

    Baselines sources

---
+++ getfakemail.php
@@ -0,0 +1,53 @@
+<?php
+ 
+  // Set default timezone
+  date_default_timezone_set('UTC');
+  header("Expires: Fri, 01 Jan 2010 05:00:00 GMT");
+  if(strstr($_SERVER["HTTP_USER_AGENT"],"MSIE")==false) {
+    header("Cache-Control: no-cache");
+    header("Pragma: no-cache");
+  }
+ 
+  try {
+    /**************************************
+    * Create databases and                *
+    * open connections                    *
+    **************************************/
+ 
+    // Create (connect to) SQLite database in file
+    $file_db = new PDO('sqlite:/tmp/_fakemailbox');
+    // Set errormode to exceptions
+    $file_db->setAttribute(PDO::ATTR_ERRMODE, 
+                            PDO::ERRMODE_EXCEPTION);
+
+    if ( isset($_GET['kill']) ) {
+      $del = "DELETE FROM mail WHERE id=:id";
+      $stmt = $file_db->prepare($del);
+      $stmt->bindValue(':id', $_GET['kill'], SQLITE3_INTEGER);
+      $stmt->execute();
+    }
+
+      // Select all data from file db messages table 
+    $result = $file_db->query('SELECT * FROM mail');
+    $output=array();
+    foreach ($result as $m) {
+      $output[] = array(
+        'id'=>$m['id'],
+        'created_at'=>$m['created_at'],
+        'mail'=>base64_decode($m['message']),
+      );
+    }    
+    echo json_encode($output);
+
+    /**************************************
+    * Close db connections                *
+    **************************************/
+ 
+    // Close file db connection
+    $file_db = null;
+
+ 
+  } catch(PDOException $e) {
+    // Print PDOException message
+    echo $e->getMessage();
+  }
---
+++ readfakemail.php
@@ -0,0 +1,92 @@
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script>
+$( document ).ready( function() {
+
+$('h2').on('click',function(e) {
+
+  $.getJSON( "getfakemail.php?"+Math.random(), function( data ) {
+    $("#mailbox").fadeOut();
+    var items = [];
+    $.each( data, function( key, val ) {
+      items.push( "<li data-id='" + val.id + "'><span class='created_at'><div class='killme'>x</div>" +val.created_at +"</span>"+ decodeURIComponent(val.mail) + "</li>" );
+    });
+
+    $("#mailbox").html( "<ul>"+items.join("")+"</ul>" );
+    $("#mailbox").fadeIn();
+  });
+
+});
+
+$("h2").trigger("click");
+
+$('body').on('click','.killme',function(e) {
+  var parentid=$(e.target).parents('li').data('id');
+
+  $.getJSON( "getfakemail.php?kill="+parentid, function( data ) {
+    $("#mailbox").fadeOut();
+    var items = [];
+    $.each( data, function( key, val ) {
+      items.push( "<li data-id='" + val.id + "'><span class='created_at'><div class='killme'>x</div>" +val.created_at +"</span>"+ decodeURIComponent(val.mail) + "</li>" );
+    });
+
+    $("#mailbox").html( "<ul>"+items.join("")+"</ul>" );
+    $("#mailbox").fadeIn();
+  });
+});
+
+
+});
+</script>
+
+<h2>Fake Mailbox</h2>
+<div class="wrapper">
+<div id="mailbox">
+</div>
+</div>
+<style>
+body {
+  padding: 1em;
+}
+h2 {
+  margin-bottom: 2em;
+}
+#mailbox {
+  padding: 1em;
+  border: solid thin red;
+  border-radius: 5px;
+  box-shadow: 0px 0px 200px black;
+}
+.wrapper {
+  box-shadow: 0px 0px 200px black;
+  border-radius: 5px;
+}
+.killme {
+  border: solid thin red;
+  border-radius: 10px;
+  height: 1.4em;
+  width: 1.4em;
+  float: right;
+  display: block;
+  text-align: center;
+}
+ul li {
+  list-style-type: none;
+  padding: 1em;
+  margin-left: 1em;
+  border: solid thin black;
+  border-radius: 5px;
+  box-shadow: 0px 0px 20px black;
+}
+
+ul li span:first-child {
+  border: solid thick black;
+  background: white;
+  position: relative;
+  top: -1.5em;
+  padding: 1em;
+  border-radius: 5px;
+  border-top: none;
+  border-bottom: none;
+}
+
+</style>
